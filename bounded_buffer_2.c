a1d81348b972681d2f4c851ca8a69d3aa55ef136
passthesalt <austinlee@Austins-MacBook-Air.local>
2018-04-16 19:25:42 -0400

    changed ensure

--- bounded_buffer_2.c
+++ bounded_buffer_2.c
@@ -21,6 +21,7 @@ int* buffer;
 sem_t empty;
 sem_t full;
 sem_t mutex;
+pthread_mutex_t m     = PTHREAD_MUTEX_INITIALIZER;
 
 void ensure(int expression, char *msg) {
   if (expression == 0) {
@@ -40,7 +41,7 @@ void put(int value, int id) {
 
 int get(int id) {
   int tmp = buffer[cons_ptr];                                 // Store the value to be consumed at the idx pointed to by the cons ptr in tmp
-  ensure(tmp != -1, "ERROR: tried to get an empty buffer");   // Make sure that the value is not -1 (signifies empty entry in buffer)
+  ensure(tmp == -1, "ERROR: tried to get an empty buffer");   // Make sure that the value is not -1 (signifies empty entry in buffer)
   buffer[cons_ptr] = -1;                                      // Render the value at the idx pointed to by cons ptr -1, value signifying empty
   cons_ptr = (cons_ptr + 1) % buffer_size;                    // Advance the cons ptr
   if (tmp != -2)
